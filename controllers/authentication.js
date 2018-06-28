5877749e5a337781f3edb21d97ce2993afbadc96
Liam <lw@esyoil.com>
2018-06-29 00:02:53 +0200

    Add signup logic with callbacks :c

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,28 @@
+const User = require("../models/user");
+
 exports.signup = (req, res, next) => {
-  res.send({ msg: "Hello!" });
+  const { email, password } = req.body;
+
+  User.findOne({ email }, (err, existingUser) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (existingUser) {
+      return res.status(422).send({ error: "Email is already in use." });
+    }
+
+    const user = new User({
+      email,
+      password
+    });
+
+    user.save(err => {
+      if (err) {
+        return next(err);
+      }
+
+      return res.send({ success: true });
+    });
+  });
 };
