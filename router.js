d3a1d334941da39fc17bc04092331b9885b88c04
Liam <lw@esyoil.com>
2018-06-30 01:30:51 +0200

    Add requireAuth

--- router.js
+++ router.js
@@ -1,8 +1,14 @@
+require("./services/passport");
+const passport = require("passport");
 const express = require("express");
+const Authentication = require("./controllers/authentication");
 const router = express.Router();
 
-const Authentication = require("./controllers/authentication");
+const requireAuth = passport.authenticate("jwt", { session: false });
 
+router.get("/", requireAuth, (req, res) => {
+  res.send({ msg: "Hello!" });
+});
 router.post("/signup", Authentication.signup);
 
 module.exports = router;
