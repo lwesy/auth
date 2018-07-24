4b0803ebc29d1003f4ef0e008a551d30eced874d
Liam <lw@esyoil.com>
2018-07-24 22:42:58 +0200

    Show links depending on auth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default class extends Component {
+class Header extends Component {
+  renderLinks() {
+    if (this.props.auth) {
+      return (
+        <div>
+          <Link to="/signout">Sign Out</Link>
+          <Link to="/feature">Feature</Link>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/signin">Sign In</Link>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
         <Link to="/">Redux Auth</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signout">Sign Out</Link>
-        <Link to="/feature">Feature</Link>
+        {this.renderLinks()}
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({ auth: state.auth.authenticated });
+
+export default connect(mapStateToProps)(Header);
