d1d6ceb6b07251a8f427539853f83e7d9ff64774
Liam <lw@esyoil.com>
2018-07-24 20:54:47 +0200

    Add redux-form

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
-export default class extends Component {
+class Signup extends Component {
   render() {
     return (
       <form>
         <fieldset>
           <label>Email</label>
+          <Field name="email" type="text" component="input" />
         </fieldset>
         <fieldset>
           <label>Password</label>
+          <Field name="password" type="password" component="input" />
         </fieldset>
       </form>
     );
   }
 }
+
+export default reduxForm({ form: "signup" })(Signup);
