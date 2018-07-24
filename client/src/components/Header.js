0437b1f8443d8daa04d0adf891e4897222fc622e
Liam <lw@esyoil.com>
2018-07-24 22:45:56 +0200

    Add some styles to the Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,32 +1,42 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const NavLink = styled(Link)`
+  margin: 0 1rem;
+`;
 
 class Header extends Component {
   renderLinks() {
     if (this.props.auth) {
       return (
         <div>
-          <Link to="/signout">Sign Out</Link>
-          <Link to="/feature">Feature</Link>
+          <NavLink to="/signout">Sign Out</NavLink>
+          <NavLink to="/feature">Feature</NavLink>
         </div>
       );
     }
 
     return (
       <div>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/signin">Sign In</Link>
+        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink to="/signin">Sign In</NavLink>
       </div>
     );
   }
 
   render() {
     return (
-      <div>
-        <Link to="/">Redux Auth</Link>
+      <Nav>
+        <NavLink to="/">Redux Auth</NavLink>
         {this.renderLinks()}
-      </div>
+      </Nav>
     );
   }
 }
